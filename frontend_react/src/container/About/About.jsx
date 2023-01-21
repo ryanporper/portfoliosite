8b83000f40b90240f9eacae9283f75603914eb7f
ryanporper <ryan_porper@aol.com>
2023-01-21 03:29:17 -0500

    typo

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -15,7 +15,7 @@ const About = () => (
       Hi, my name is Ryan. I am a highly motivated software engineer graduate from  
       <a href='https://www.codingdojo.com/online-coding-bootcamp-full-time' alt='coding dojo'> Coding Dojo. </a> 
       Ever since I was a little kid I had an interest in coding. It all started from me playing videos games. I wondered how I could make one myself, so I googled it
-      and from there on began my journey into programming. After some time of messing with code myself I attending Palm Beach State College where I studied Computer Science to
+      and from there on began my journey into programming. After some time of messing with code myself I attended Palm Beach State College where I studied Computer Science to
       ofically start my career path. After my adventures at PBSC I found Coding Dojo. They did not just seem like every other coding boot camp I have seen before. So I applied 
       for their next cohort and got in! After a intuitive 14 week 3 stack program I graduated with a certificate in Software Developent. Since then I have continued to expand
       my knowledge with my own research and projects. I work great in a team environments but I can also do great work alone. Very eager to grow and learn new things. Fast learner
