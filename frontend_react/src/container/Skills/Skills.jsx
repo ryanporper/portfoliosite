2608316a5f510ea2d5cfadd0dfc7ab30f283d2c3
ryanporper <ryan_porper@aol.com>
2023-01-25 01:01:29 -0500

    added experiences

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -6,10 +6,17 @@ import { urlFor, client } from '../../client';
 import './Skills.scss';
 
 const Skills = () => {
+  const [experiences, setExperiences] = useState([]);
   const [skills, setSkills] = useState([]);
 
   useEffect(() => {
+    const query = '*[_type == "experiences"]';
     const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query).then((data) => {
+      setExperiences(data);
+    });
+
     client.fetch(skillsQuery).then((data) => {
       setSkills(data);
     });
@@ -17,12 +24,10 @@ const Skills = () => {
 
   return (
     <>
-      <h2 className="head-text">Skills</h2>
+      <h2 className="head-text">Skills & Experiences</h2>
+
       <div className="app__skills-container">
-        <motion.div 
-          className="app__skills-list"
-          whileInView={{ scale: [0, 1] }}
-          transition={{ duration: 1.0, ease: 'easeInOut' }}>
+        <motion.div className="app__skills-list">
           {skills.map((skill) => (
             <motion.div
               whileInView={{ opacity: [0, 1] }}
@@ -40,6 +45,34 @@ const Skills = () => {
             </motion.div>
           ))}
         </motion.div>
+        <div className="app__skills-exp">
+          {experiences.map((experience) => (
+            <motion.div
+              className="app__skills-exp-item"
+              key={experience.year}
+            >
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+              <motion.div className="app__skills-exp-works">
+                {experience.works.map((work) => (
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                      data-tip
+                      data-for={work.name}
+                      key={work.name}
+                    >
+                      <h4 className="bold-text">{work.name}</h4>
+                      <p className="p-text">{work.company}</p>
+                      <p className="p-text">{work.desc}</p>
+                    </motion.div>
+                ))}
+              </motion.div>
+            </motion.div>
+          ))}
+        </div>
       </div>
     </>
   );
